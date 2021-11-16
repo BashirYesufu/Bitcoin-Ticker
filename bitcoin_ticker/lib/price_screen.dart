460b2614a1556f7fc717c6cee931acb0e13678a4
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-16 01:16:47 +0100

    Used ternary operator to show a picker according to platform

--- bitcoin_ticker/lib/price_screen.dart
+++ bitcoin_ticker/lib/price_screen.dart
@@ -53,15 +53,6 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
-// Checking if the platform is iOS or android
-  Widget getPicker() {
-    if(Platform.isIOS) {
-      return iOSPicker();
-    }
-    return androidDropDownButton();
-}
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,7 +89,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: getPicker(),
+            child: Platform.isIOS ? iOSPicker() : androidDropDownButton(),
             ),
         ],
       ),
