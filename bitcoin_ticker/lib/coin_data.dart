648a30059e088d75ac181bf1372b48c264c13a7d
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-16 10:05:43 +0100

    bronze level

--- bitcoin_ticker/lib/coin_data.dart
+++ bitcoin_ticker/lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,26 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'C1B63352-74DF-4CA7-8F11-874E63F91FE3';
+
+
+class CoinData {
+
+  Future getCoinData() async {
+
+    String requestUrl = '$coinAPIURL/BTC/USD?apikey=$apiKey';
+    http.Response response = await http.get(Uri.parse(requestUrl),);
+
+    if(response.statusCode == 200) {
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+      var lastPrice = decodedData['rate'];
+      return lastPrice;
+    } else  {
+      print(response.statusCode);
+      throw 'Error with get request!';
+    }
+  }
+}
--- bitcoin_ticker/lib/price_screen.dart
+++ bitcoin_ticker/lib/price_screen.dart
@@ -53,6 +53,24 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
+  String bitcoinValueInUSD = '?';
+  Future getData() async {
+    try {
+      double data = await CoinData().getCoinData();
+      setState(() {
+        bitcoinValueInUSD = data.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,7 +92,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $bitcoinValueInUSD USD',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
