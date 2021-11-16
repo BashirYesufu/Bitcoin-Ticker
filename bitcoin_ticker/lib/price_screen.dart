0b03cd8c3b413792a7f5a9ecafb562fecabab8a3
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-16 01:07:34 +0100

    Modified code o get an iOS picker and android drop down

--- bitcoin_ticker/lib/price_screen.dart
+++ bitcoin_ticker/lib/price_screen.dart
@@ -1,4 +1,5 @@
 import 'package:bitcoin_ticker/coin_data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PriceScreen extends StatefulWidget {
@@ -11,6 +12,47 @@ class PriceScreen extends StatefulWidget {
 class _PriceScreenState extends State<PriceScreen> {
 
   String selectedValue = 'USD';
+
+  // Getting data for an android drop down list
+  DropdownButton<String> androidDropDownButton() {
+    List<DropdownMenuItem<String>> items = [];
+    for(String currency in currenciesList) {
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      items.add(newItem);
+    }
+     return DropdownButton<String>(
+         value: selectedValue,
+         items: items,
+         onChanged: (value) {
+           setState(() {
+             if (value != null) {
+               selectedValue = value;
+             }
+           });
+         });
+  }
+
+  // Getting data for an iOS cupertino picker
+  CupertinoPicker iOSPicker() {
+    List<Text> items = [];
+    for(String currency in currenciesList) {
+      items.add(Text(currency));
+    }
+
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (int selectedIndex) {
+        print(selectedIndex);
+      },
+      children: items,
+    );
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -48,24 +90,8 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: DropdownButton<String>(
-              value: selectedValue,
-              items: [
-                for(String currency in currenciesList)
-                  DropdownMenuItem(
-                    child: Text(currency),
-                    value: currency,
-                  )
-              ],
-              onChanged: (value) {
-                setState(() {
-                  if (value != null) {
-                    selectedValue = value;
-                  }
-                });
-              },
+            child: iOSPicker(),
             ),
-          ),
         ],
       ),
     );
